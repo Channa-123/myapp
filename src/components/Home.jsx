eb2582648f18f67f5efba58657e2455a15057ce9
channa <96125389+Channa-123@users.noreply.github.com>
2024-02-13 06:53:33 +0000

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import "./HomeStyles.css";
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <h1>This is a React Navbar Home page.</h1>
+        <h3>Difficulty Level: 0;</h3>
+      </div>
+    </>
+  );
+}
+
+export default Home;
